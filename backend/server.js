39d39638bcec3ba9fde2e2489068c148e777c9ef
Alexis Sales <mariaalexissales@gmail.com>
2025-05-16 21:08:23 -0500

    setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend");
+})
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
